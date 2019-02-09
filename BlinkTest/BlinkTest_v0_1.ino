76465a5f44c2f3ef4bda5089aea899906133366c
Tom Ploetz <ploetztj@miamioh.edu>
2019-02-09 16:51:28 -0500

    minor refactoring

--- BlinkTest/BlinkTest_v0_1.ino
+++ BlinkTest/BlinkTest_v0_1.ino
@@ -17,29 +17,27 @@ Button Class
 creates an object that can read whether or not the button is pressed
 */
 class Button {
-//Arduino pin button is connected to
-int _pin;
-public:
-    //Constructor
-    Button(int);
-    //Functions
-    /**Reads the current state of the button
-     * @return - HIGH(1/pressed) or LOW(0/unpressed)
-     * */ 
-    int read()
-    {
-    return digitalRead(_pin);
-    }
+    //Arduino pin button is connected to
+    int _pin;
+    public:
+        //Constructor
+        Button(int);
+        //Functions
+        /**Reads the current state of the button
+         * @return - HIGH(1/pressed) or LOW(0/unpressed)
+         * */ 
+        int read() {
+            return digitalRead(_pin);
+        };
 };
 /**
  * Button Constructor
  * @pin number of pin buton is connected to
  */
-Button::Button(int pin)
-    {
-        pinMode(pin, INPUT);
-        _pin = pin;
-    }
+Button::Button(int pin) {
+    pinMode(pin, INPUT);
+    _pin = pin;
+};
 
 /*Dot Class
 Class that sets a dot in a specific locatio on the LED strip
@@ -57,8 +55,7 @@ class Dot {
     Dot(int, int, int, int);
 
     //Cycles thru colors of player
-    void colorCycle()
-    {
+    void colorCycle() {
         //store old color values        
         int oldBlue = Blue;
         int oldGreen = Green;
@@ -68,46 +65,46 @@ class Dot {
         Green = oldBlue;
         Red = oldGreen;
     //Add more colors thru map or array or something                
-    }     
+    };   
 
     //attack
-    void attack()
-    {
-        // indicate attack is charging
-        int attackChargeTime = millis()
-        for (int i=target.Loc; i<target.Loc+5; i++){
-            leds[i] = CRGB::DarkRed;
-        }
-        for (int i=target.Loc; i>target.Loc-5; i--){
-            leds[i] = CRGB::DarkRed
-        }
-        //indicate attack charge complete
-        if attackChargeTime < 2000{
-            for (int i=target.Loc; i<target.Loc+5; i++){
-                leds[i] = CRGB::Red;
-        }
-        for (int i=target.Loc; i>target.Loc-5; i--){
-                leds[i] = CRGB::Red
-        }
-        }
-        //begin attack
-        for (int i=target.Loc; i<target.Loc+15; i++){
-            leds[i] = CRGB::White;
-        }
-        for (int i=target.Loc; i>target.Loc-15; i--){
-            leds[i] = CRGB::White
-        }
-        }
-        //end attack
-        for (int i=target.Loc; i<target.Loc+15; i++){
-             //fade to black by 25% per iteration
-             leds[i].fadeToBlackBy( 64 );
-        }
-        for (int i=target.Loc; i>target.Loc-15; i--){
-             leds[i].fadeToBlackBy( 64 );
-        }
-        }
-    }   
+    // void attack()
+    // {
+    //     // indicate attack is charging
+    //     int attackChargeTime = millis()
+    //     for (int i=target.Loc; i<target.Loc+5; i++){
+    //         leds[i] = CRGB::DarkRed;
+    //     }
+    //     for (int i=target.Loc; i>target.Loc-5; i--){
+    //         leds[i] = CRGB::DarkRed
+    //     }
+    //     //indicate attack charge complete
+    //     if attackChargeTime < 2000{
+    //         for (int i=target.Loc; i<target.Loc+5; i++){
+    //             leds[i] = CRGB::Red;
+    //     }
+    //     for (int i=target.Loc; i>target.Loc-5; i--){
+    //             leds[i] = CRGB::Red
+    //     }
+    //     }
+    //     //begin attack
+    //     for (int i=target.Loc; i<target.Loc+15; i++){
+    //         leds[i] = CRGB::White;
+    //     }
+    //     for (int i=target.Loc; i>target.Loc-15; i--){
+    //         leds[i] = CRGB::White
+    //     }
+    //     }
+    //     //end attack
+    //     for (int i=target.Loc; i<target.Loc+15; i++){
+    //          //fade to black by 25% per iteration
+    //          leds[i].fadeToBlackBy( 64 );
+    //     }
+    //     for (int i=target.Loc; i>target.Loc-15; i--){
+    //          leds[i].fadeToBlackBy( 64 );
+    //     }
+    //     }
+    //}   
 
 };
 /**
@@ -117,14 +114,22 @@ class Dot {
  * @param green - green value (0-255)
  * @param blue - blue value (0-255)
  * */
-Dot::Dot(int loc, int red, int green, int blue)
-{
+Dot::Dot(int loc, int red, int green, int blue) {
     Loc = loc;
     Red = red;
     Green = green;
     Blue = blue;
     LocUp = true;
 }
+
+// class Game {
+//     public: 
+//         Game()
+
+//         bool Restart() {
+
+//         }
+// }
 
 //Making Objects
 // Buttons 
@@ -137,6 +142,7 @@ Button Down(8);
 // Dots on Strip
 Dot player(0,255,0,0);
 Dot target(10,0,0,255);
+Dot enemy(0,255,0,0);
 // Other variables
 int redColor = 0;
 int blueColor = 0;
@@ -144,67 +150,30 @@ int greenColor = 0;
 int buttonUpState = 0; 
 int buttonDownState = 0;
 int buttonBState = 0;
+int gameState = 0;
 //LED strip
 // This is an array of leds.  One item for each led in your strip.
 CRGB leds[NUM_LEDS];
 
-void setup() {
-//??NOT SURE WHAT THIS DOES BUT IS IN A LOT OF EXAMPLE CODE??? - JON
-// Serial.begin(4800);
-// while (!Serial) {
-//  ; // wait for serial port to connect. Needed for native USB
-//}
-	// sanity check delay - allows reprogramming if accidently blowing power w/leds
-   	delay(2000);
-    FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
-    // initialize the pushbutton pin as an input:
-    //Dot player(0,255,0,0);
-    //Dot target(10,0,0,255);
-    leds[player.Loc].setRGB( player.Red, player.Green, player.Blue); // Player.
-    leds[target.Loc].setRGB(target.Red, target.Green, target.Blue); // Player.
-    //Many options for ways to set color see below
-    //https://github.com/FastLED/FastLED/wiki/Controlling-leds
-    FastLED.show();
-}
+void checkWin();
 
-void loop() {
-    delay(delayval);
+void checkWin() {
+    if(gameState == 0) {
+        // do nothing
+    } else if (gameState == 2) {
+        // Lose state
+        for (int i = 0; i < NUM_LEDS; i++){
+            leds[i].setRGB( 255, 0, 0);
+            FastLED.show();
+            delay(animationDelay);
+        }
 
-    // read the state of the pushbutton value:
-    buttonUpState = Up.read();
-    buttonDownState = Down.read();
-    buttonBState = B.read();
-
-    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
-    // If Button is pressed, current Dot is removed, and player location is +1'd
-    if (buttonUpState == HIGH && player.Loc < (NUM_LEDS-1)) {
-        // move player up:
-        leds[player.Loc] = CRGB::Black; //erase old location
-        player.Loc++;
-        player.LocUp = true;
-    }
-    buttonDownState = Down.read();
-    if (buttonDownState == HIGH && player.Loc > 0) {
-        // move player down:
-        leds[player.Loc] = CRGB::Black; //erase old location
-        player.Loc--;
-        player.LocUp = false;
-    }
-    // Read B Button for color change
-    if (buttonBState == HIGH) {
-        // move player down:
-        player.colorCycle();
-    }
-
-    //Reset pixel locations to current locations (i.e. if Button was pressed)
-    leds[player.Loc].setRGB( player.Red, player.Green, player.Blue); // Player.
-    leds[target.Loc].setRGB(target.Red, target.Green, target.Blue); // Target (not used unless target has move conditions)
-    FastLED.show();
-
-//Win state
-    if (player.Loc == target.Loc) {
-        if(player.LocUp)
-        {
+        //Restart game
+        player.Loc = random(0,NUM_LEDS);
+        target.Loc = random(0,NUM_LEDS);
+    } else if (gameState == 1) {
+        //Win state
+        if(player.LocUp) {
             for (int i=player.Loc; i<NUM_LEDS; i++){
                 int redColor = random(0,255);
                 int greenColor = random(0,255);
@@ -254,8 +223,76 @@ void loop() {
             }
         }
 
-    //Restart game
-    player.Loc = random(0,NUM_LEDS);
-    target.Loc = random(0,NUM_LEDS);
+        //Restart game
+        player.Loc = random(0,NUM_LEDS);
+        target.Loc = random(0,NUM_LEDS);
     }
-}
+};
+
+void setup() {
+    // sanity check delay - allows reprogramming if accidently blowing power w/leds
+    delay(2000);
+    FastLED.addLeds<WS2812B, LED_PIN, RGB>(leds, NUM_LEDS);
+    // initialize the pushbutton pin as an input:
+    //Dot player(0,255,0,0);
+    //Dot target(10,0,0,255);
+    leds[player.Loc].setRGB( player.Red, player.Green, player.Blue); // Player.
+    leds[target.Loc].setRGB(target.Red, target.Green, target.Blue); // Player.
+    leds[enemy.Loc].setRGB(enemy.Red, enemy.Green, enemy.Blue) // Enemy
+    //Many options for ways to set color see below
+    //https://github.com/FastLED/FastLED/wiki/Controlling-leds
+    FastLED.show();
+};
+
+void loop() {
+    delay(delayval);
+
+    // read the state of the pushbutton value:
+    buttonUpState = Up.read();
+    buttonDownState = Down.read();
+    buttonBState = B.read();
+
+    // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+    // If Button is pressed, current Dot is removed, and player location is +1'd
+    if (buttonUpState == HIGH && player.Loc < (NUM_LEDS-1)) {
+        // move player up:
+        leds[player.Loc] = CRGB::Black; //erase old location
+        player.Loc++;
+        player.LocUp = true;
+    };
+    buttonDownState = Down.read();
+    if (buttonDownState == HIGH && player.Loc > 0) {
+        // move player down:
+        leds[player.Loc] = CRGB::Black; //erase old location
+        player.Loc--;
+        player.LocUp = false;
+    };
+    // Read B Button for color change
+    if (buttonBState == HIGH) {
+        // move player down:
+        player.colorCycle();
+    };
+
+    // target movement
+    int targetRandom = random(0, 100);
+    if(targetRandom < 5){
+        pixels.setPixelColor(target.Loc, blankPixel);
+        if(player.Loc > target.Loc && target.Loc < (NUMPIXELS-1)){
+            target.Loc--;
+        }else if (target.Loc > 0){
+            target.Loc++;
+        }
+    }
+
+    //Reset pixel locations to current locations (i.e. if Button was pressed)
+    leds[player.Loc].setRGB( player.Red, player.Green, player.Blue); // Player.
+    leds[target.Loc].setRGB(target.Red, target.Green, target.Blue); // Target (not used unless target has move conditions)
+    FastLED.show();
+
+    if (player.Loc == target.Loc) {
+        gameState = 1;
+    } else if (player.Loc == enemy.Loc) {
+        gameState = 2;
+    };
+    checkWin(gameState);
+};
