db1d8919d79ec699cb8885f61ed5c1d9e5cb76c1
Robby <blumrj@gmail.com>
2019-04-11 16:55:43 -0400

    n fireworks!

--- fireworkTest/fireworkTest.ino
+++ fireworkTest/fireworkTest.ino
@@ -90,7 +90,9 @@ void setup() {
     //set player on strip
     
     writeBackground();
-    leds[firework[f].Loc].setRGB(firework[f].Green,firework[f].Red,firework[f].Blue);
+ for (int f = 0; f < NUM_FIREWORKS; f++){
+     leds[firework[f].Loc].setRGB(firework[f].Green,firework[f].Red,firework[f].Blue);
+ }
     //leds[firework2.Loc].setRGB(firework2.Green,firework2.Blue,firework2.Red);
     //leds[firework3.Loc].setRGB(firework3.Green,firework3.Blue,firework3.Red);
 
@@ -98,10 +100,11 @@ void setup() {
 }
 
 //Game Loop
-void loop() {
+void loop() 
+{
 
-    //remove old fireworks & flames
-    writeBackground();
+//remove old fireworks & flames
+writeBackground();
 
 for (int f = 0; f < NUM_FIREWORKS; f++){
     firework[f].Move();
@@ -137,75 +140,5 @@ for (int f = 0; f < NUM_FIREWORKS; f++){
         firework[f].Reset();
     }
 }
-
-
-/*     firework2.Move();
-    //write firework2
-    leds[firework2.Loc].setRGB(firework2.Green,firework2.Red,firework2.Blue);
-    
-    if (firework2.Exploded == false){
-    writeFlame(firework2.Loc);
-    } else {
-        redColor = random(0,255);
-        greenColor = random(0,255);
-        blueColor = random(0,255);
-        leds[firework2.Loc].setRGB(greenColor,redColor,blueColor);
-        if (firework2.burstFin == false){
-            for (int i = firework2.Loc; i < firework2.Loc + firework2.loopCounter; i++ ){
-                leds[i].setRGB(greenColor,redColor,blueColor);
-            }
-        for (int i = firework2.Loc; i > firework2.Loc - firework2.loopCounter; i-- ){
-                leds[i].setRGB(greenColor,redColor,blueColor);
-            }
-            if (firework2.slowDown >= 2){
-                firework2.loopCounter = firework2.loopCounter + 1;
-                firework2.slowDown = 0;
-            } else {firework2.slowDown = firework2.slowDown + 1;
-            }
-        if (firework2.loopCounter == 12){
-            firework2.burstFin = true;
-        }
-        }
-    }
-    //reset fireworks if burst animation has completed
-    if (firework2.burstFin == true){
-        firework2.Reset();
-    }
-
-
-
-
-    firework3.Move();
-    //write firework3
-    leds[firework3.Loc].setRGB(firework3.Green,firework3.Red,firework3.Blue);
-    
-    if (firework3.Exploded == false){
-    writeFlame(firework3.Loc);
-    } else {
-        redColor = random(0,255);
-        greenColor = random(0,255);
-        blueColor = random(0,255);
-        leds[firework3.Loc].setRGB(greenColor,redColor,blueColor);
-        if (firework3.burstFin == false){
-            for (int i = firework3.Loc; i < firework3.Loc + firework3.loopCounter; i++ ){
-                leds[i].setRGB(greenColor,redColor,blueColor);
-            }
-        for (int i = firework3.Loc; i > firework3.Loc - firework3.loopCounter; i-- ){
-                leds[i].setRGB(greenColor,redColor,blueColor);
-            }
-            if (firework3.slowDown >= 2){
-                firework3.loopCounter = firework3.loopCounter + 1;
-                firework3.slowDown = 0;
-            } else {firework3.slowDown = firework3.slowDown + 1;
-            }
-        if (firework3.loopCounter == 12){
-            firework3.burstFin = true;
-        }
-        }
-    }
-    //reset fireworks if burst animation has completed
-    if (firework3.burstFin == true){
-        firework3.Reset();
-    } */
-        FastLED.show();
-        };
+FastLED.show();
+};
