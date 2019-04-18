f4c6a53499abf99d75257705848d07ec6f4fb4c6
mj0ln1r42 <baileybp4@gmail.com>
2019-04-17 22:58:27 -0400

    Change Rainbow5 to calculate each pixel's color in time rather than basing off of an offset array

--- Animations/Rainbow5/Rainbow5.ino
+++ Animations/Rainbow5/Rainbow5.ino
@@ -1,13 +1,16 @@
 #include <FastLED.h>
-#define PIN 1   // OUTPUT pin WS2812B LED Strip is attached to TODO: Rename to LED_PIN
+
 #define debounceTime 200 // keep those button inputs clean
 #define NUM_STRIPS 5    //number of LED strips
 #define NUM_LEDS 300    //number of LEDs per strip
+
+// LED Strip Pin Addresses
 #define STRIP1_PIN 2    
 #define STRIP2_PIN 3
 #define STRIP3_PIN 4
 #define STRIP4_PIN 5
 #define STRIP5_PIN 6
+
 #define NUM_COLORS 7    //number of colors in da rainbow :)
 
 #define delayval 25 //controls the "speed" of the player dot
@@ -28,19 +31,10 @@ CRGB rainbows[NUM_COLORS] = {
   CRGB::Violet
 };
 
-//int iterator = 0;
-int stripColorOffset[NUM_STRIPS];
-
-
 void setup() { 
     //FastLED.addLeds<NEOPIXEL, PIN>(leds, NUM_LEDS); 
     // sanity check delay - allows reprogramming if accidently blowing power w/leds
    	delay(2000);
-
-    //Populate iterator array
-    for (int i = 0; i < NUM_STRIPS; i++) {
-        stripColorOffset[i] = i;
-    }
 
     //This initializes the LED NUM_STRIPS
     FastLED.addLeds<WS2812B, STRIP1_PIN, RGB>(leds[0], NUM_LEDS);
@@ -53,18 +47,23 @@ void setup() {
     FastLED.show();
 }
 
+int frameIndex = 0;
+
 void loop(){
     for (int ledIndex = 0; ledIndex < NUM_LEDS ; ledIndex++) {
 		for (int stripIndex = 0; stripIndex < NUM_STRIPS; stripIndex++) {
-			int colorIndex = (ledIndex + stripColorOffset[stripIndex]) % NUM_COLORS;
-			leds[stripIndex][ledIndex] = rainbows[colorIndex];
+			leds[stripIndex][ledIndex] = *getBackgroundColor(frameIndex, stripIndex, ledIndex);
 		}
     }
 	
-    for (int stripIndex = 0; stripIndex < NUM_STRIPS; stripIndex++) {
-		stripColorOffset[stripIndex] = (stripColorOffset[stripIndex] + 1) % NUM_COLORS;
-    }
-	
+    frameIndex = (frameIndex + 1) % NUM_COLORS;
+
     FastLED.show();
     delay(delayval);
+}
+
+inline CRGB* getBackgroundColor(int frame, int strip, int led)
+{
+    int colorIndex = (led + frame + strip) % NUM_COLORS;
+    return &rainbows[colorIndex];
 }
